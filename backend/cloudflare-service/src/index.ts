4e8438d2f4d2c3895d5110d897c9c1c38c1a4312
Onebluesky882 <onebluesky882@outlook.com>
2026-06-02 14:13:08 +0700

    build : auth

--- backend/cloudflare-service/src/index.ts
+++ backend/cloudflare-service/src/index.ts
@@ -1,20 +1,42 @@
 import { Hono } from "hono";
 import { createDb } from "./db";
 import { posts } from "./db/schema";
+import { drizzle } from "drizzle-orm/d1";
+import { createAuth } from "./lib/auth";
 
 
 type Bindings = {
 
-  DB: D1Database;
+  DATABASE: D1Database;
+  BETTER_AUTH_SECRET: string;
+  BETTER_AUTH_URL: string;
 
 };
 const app = new Hono<{ Bindings: Bindings }>();
 
+app.all("/api/auth/*", async (c) => {
+  try {
+    const db = drizzle(c.env.DATABASE);
+
+    const auth = createAuth(db);
+
+    return auth.handler(c.req.raw);
+  } catch (error) {
+    console.error("AUTH ERROR:", error);
+
+    return c.json(
+      {
+        error: String(error),
+      },
+      500
+    );
+  }
+});
  
 
 app.get("/posts", async (c) => {
 
-  const db = createDb(c.env.DB);
+  const db = createDb(c.env.DATABASE);
 
   const result = await db
 
